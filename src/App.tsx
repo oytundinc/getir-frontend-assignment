cf66bfb83f64ef3201c33054b1595ecd47d1008b
Oytun DİNÇ <77075563+oytundinc@users.noreply.github.com>
2022-10-27 20:53:29 +0300

    add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./App.css";
+import { Button } from "./components/button/button";
+
+
 
 function App() {
   return (
     <div className="App">
+      <Button className="oytun">oytun</Button>
     </div>
   );
 }
---
+++ src/components/button/button.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+const ButtonStyled = styled.div`
+  button {
+    font-weight: 600;
+    font-size: 12px;
+    
+    color: #ffffff;
+    border: none;
+    border-radius: 2px;
+    background-color: #1ea4ce;
+  }
+`;
+
+interface ButtonProps {
+  children: string;
+  className: string;
+}
+
+const WrappedButton = ({ children, className }: ButtonProps) => {
+  return (
+    <ButtonStyled>
+      <button className={className}>{children}</button>
+    </ButtonStyled>
+  );
+};
+
+export const Button = React.memo(WrappedButton);
