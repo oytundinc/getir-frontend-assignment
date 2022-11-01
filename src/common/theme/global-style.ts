77cb082f4d3bad63b28bc4967588885e239320f5
Oytun DİNÇ <77075563+oytundinc@users.noreply.github.com>
2022-11-02 01:56:18 +0300

    update style

--- src/common/theme/global-style.ts
+++ src/common/theme/global-style.ts
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    @import url(‘https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600&display=swap');
     body {
         margin:0;
         padding:0;
